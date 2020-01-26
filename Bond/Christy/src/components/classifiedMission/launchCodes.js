0f5a756d1224cc35756088a730de195e642a2fd7
eddieruiz <eddi@overseasinternational.com>
2020-01-26 16:24:07 -0500

    showing agent name on front end

--- Bond/Christy/src/components/classifiedMission/launchCodes.js
+++ Bond/Christy/src/components/classifiedMission/launchCodes.js
@@ -4,7 +4,18 @@ export default class launchCodes extends Component {
   render() {
     return (
       <div>
-        hello
+        <div className="row">
+          <form className="col-sm-12">
+            <div className="row">
+              <div className="input-field col-sm-6">
+                <label for="vaultDoor">Agent:</label>
+                <input id="vaultDoor" value="Bond,James" type="text" className="validate"/>
+        
+              </div>
+            </div>
+          </form>
+          
+        </div>
       </div>
     )
   }
